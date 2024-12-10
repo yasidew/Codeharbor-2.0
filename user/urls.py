8779cfbaeeadc2e681326e09629b73caf5288e75
it21312908 <it21312908@my.sliit.lk>
2024-12-10 09:43:53 +0530

    accessibility score implementation deletion

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserAPI, logout, logout_all
@@ -10,5 +11,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user', UserAPI.as_view()),
     path('api/logout', logout),
-    path('api/logout-all', logout_all)
+    path('api/logout-all', logout_all),
+    path('login/', lambda request: render(request, 'login.html'), name='login'),
+    path('dashboard/', lambda request: render(request, 'dashboard.html'), name='login'),
 ]
