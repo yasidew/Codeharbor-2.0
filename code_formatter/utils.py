4bac6fc52ab32749388a97f5c0990b4e358ab6df
IT21440236 <it21440236@my.sliit.lk>
2025-03-16 10:10:34 +0530

    Yeran.K | singelton model removed

--- code_formatter/utils.py
+++ code_formatter/utils.py
@@ -5,23 +5,6 @@ import openai
 # Base directory for the project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Path to the trained model
-MODEL_PATH = os.path.join(BASE_DIR, "code-refactoring", "singleton_model")
-
-# Load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_PATH)
-
-def format_code_with_model(code):
-    """
-    Function to format code using the trained model.
-    """
-    inputs = tokenizer(code, return_tensors="pt", truncation=True, padding="max_length", max_length=512)
-    outputs = model.generate(**inputs, max_length=512, num_beams=4, early_stopping=True)
-    formatted_code = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return formatted_code
-
-
 def refactor_code(code):
     """
     Uses OpenAI to refactor the given code and return a cleaned-up version.
